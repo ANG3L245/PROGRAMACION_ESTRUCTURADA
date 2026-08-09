d36d4dcdf9adda504ff029d17e41bd96600ec975
ANG3L245 <angelzafa245@gmail.com>
2026-08-09 17:40:25 -0600

    Delete proyecto_final1 directory

--- proyecto_final1/proyecto_final1/usuarios/seguridad.py
+++
@@ -1,9 +0,0 @@
-import bcrypt
-
-
-def generarHash(contrasena):
-    return bcrypt.hashpw(contrasena.encode(), bcrypt.gensalt()).decode()
-
-
-def verificarHash(contrasena, hash_guardado):
-    return bcrypt.checkpw(contrasena.encode(), hash_guardado.encode())
